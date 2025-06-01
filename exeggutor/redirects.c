6e9ec4f2f21e5dff54845ad87c5da6e518150878
Alejandro Hernández Hernández <alejandro1998hernandez@gmail.com>
2025-06-01 11:32:17 +0200

    norminette

--- exeggutor/redirects.c
+++ exeggutor/redirects.c
@@ -12,60 +12,62 @@
 
 #include "../minishell.h"
 
-void transfer_output(int *fds, char *output)
+void	transfer_output(int *fds, char *output)
 {
-    if (!fds)
-    {
-        if (!ft_putendl_fd(output, STDOUT_FILENO))
-            ft_perror_exit("Error writing to standard output");
-        return ;
-    }
-    while (*fds != -1)
-    {
-        if (!ft_putendl_fd(output, *fds))
-            ft_perror_exit("Error writing to output file");
-        close(*(fds++));
-    }
+	if (!fds)
+	{
+		if (!ft_putendl_fd(output, STDOUT_FILENO))
+			ft_perror_exit("Error writing to standard output");
+		return ;
+	}
+	while (*fds != -1)
+	{
+		if (!ft_putendl_fd(output, *fds))
+			ft_perror_exit("Error writing to output file");
+		close(*(fds++));
+	}
 }
 
-int *get_output_files(t_list *redirects)
+int	*get_output_files(t_list *redirects)
 {
-    int				*fds;
-    t_redirect		*redirect;
-    unsigned int	index;
+	int				*fds;
+	t_redirect		*redirect;
+	unsigned int	index;
 
-    if (!redirects)
-        return (NULL);
-    fds = malloc(sizeof(int) * (ft_lstsize(redirects) + 1));
-    index = 0;
-    while (redirects)
-    {
-        redirect = redirects->content;
-        if (redirect->is_double)
-            fds[index] = open(redirect->path, O_WRONLY | O_CREAT | O_APPEND, 0644);
-        else
-            fds[index] = open(redirect->path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-        if (fds[index++] == -1)
-            ft_perror_exit("Error opening output file");
-        redirects = redirects->next;
-    }
-    fds[index] = -1;
-    return (fds);
+	if (!redirects)
+		return (NULL);
+	fds = malloc(sizeof(int) * (ft_lstsize(redirects) + 1));
+	index = 0;
+	while (redirects)
+	{
+		redirect = redirects->content;
+		if (redirect->is_double)
+			fds[index] = open(redirect->path, O_WRONLY | O_CREAT | O_APPEND,
+					0644);
+		else
+			fds[index] = open(redirect->path, O_WRONLY | O_CREAT | O_TRUNC,
+					0644);
+		if (fds[index++] == -1)
+			ft_perror_exit("Error opening output file");
+		redirects = redirects->next;
+	}
+	fds[index] = -1;
+	return (fds);
 }
 
-int get_input_file(t_list *redirects)
+int	get_input_file(t_list *redirects)
 {
-    int        fd;
-    t_redirect *redirect;
+	int			fd;
+	t_redirect	*redirect;
 
-    if (!redirects)
-        return (-1);
-    redirect = redirects->content;
-    fd = open(redirect->path, O_RDONLY);
-    if (fd == -1)
-        ft_perror_exit("Error opening input file");
-    if (dup2(fd, STDIN_FILENO) == -1)
-        ft_perror_exit("Error redirecting input");
-    close(fd);
-    return (fd);
+	if (!redirects)
+		return (-1);
+	redirect = redirects->content;
+	fd = open(redirect->path, O_RDONLY);
+	if (fd == -1)
+		ft_perror_exit("Error opening input file");
+	if (dup2(fd, STDIN_FILENO) == -1)
+		ft_perror_exit("Error redirecting input");
+	close(fd);
+	return (fd);
 }
