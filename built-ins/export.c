530ab2dcb840de6f50003e4d7b6e063b4fb60ba9
Alejandro Hernández Hernández <alejandro1998hernandez@gmail.com>
2025-06-09 11:35:27 +0200

    refactor: replace ft_printf_fd with ft_putendl_fd for error messages in export function

--- built-ins/export.c
+++ built-ins/export.c
@@ -16,38 +16,49 @@ static int	prevalidate_export(char *var)
 {
 	if (!var || !var[0])
 	{
-		ft_printf_fd(2, "export: not enough arguments\n");
+		ft_putendl_fd("export: not enough arguments", STDERR_FILENO);
 		return (1);
 	}
 	if (ft_strchr(var, '=') == NULL)
 	{
-		ft_printf_fd(2, "export: invalid variable format\n");
+		ft_putendl_fd("export: invalid variable format", STDERR_FILENO);
 		return (1);
 	}
 	if (ft_isdigit(var[0]))
 	{
-		ft_printf_fd(2, "export: variable name cannot start with a digit\n");
+		ft_putendl_fd("export: variable name cannot start with a digit", 2);
 		return (1);
 	}
 	if (ft_strncmp(var, "SHELL", 5) == 0)
-		return (perror("export: SHELL: cannot modify environment variable\n"),
-			1);
+	{
+		ft_putendl_fd("export: SHELL: cannot modify environment variable", STDERR_FILENO);
+		return	(1);
+	}
 	return (0);
 }
 
 int	builtin_export(char **command, char ***env)
 {
 	char	*var;
+	char	*value;
 
 	if (prevalidate_export(command[1]))
 		return (1);
 	var = ft_strchr(command[1], '=');
 	if (var)
 		*var = '\0';
-	if (!ft_setenv(command[1], var + 1, env))
+	value = ft_strtrim(var + 1, "\"");
+	if (!value)
 	{
-		perror("export: cannot set environment variable");
+		ft_putendl_fd("export: memory allocation error", STDERR_FILENO);
 		return (1);
 	}
+	if (!ft_setenv(command[1], value, env))
+	{
+		free(value);
+		ft_putendl_fd("export: failed to set environment variable", STDERR_FILENO);
+		return (1);
+	}
+	free(value);
 	return (0);
 }
