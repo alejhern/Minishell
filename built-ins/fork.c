ae22d53b05f57d0c2ee9e302d5a619b24b1407ba
Alejandro Hernández Hernández <alejhern@student.42barcelona.com>
2025-06-28 01:11:32 +0200

    no hay que hacer fork en cd ni exit si no hay una pipe

--- built-ins/fork.c
+++ built-ins/fork.c
@@ -21,12 +21,37 @@ static void	close_fork(t_redirs_manage *redirs_manage, pid_t pid)
 		ft_perror_exit("restore stdout");
 }
 
+static void	close_pipes(t_redirs_manage *redirs_manage)
+{
+	if (redirs_manage->pipes[0] != -1)
+		close(redirs_manage->pipes[0]);
+	if (redirs_manage->pipes[1] != -1)
+		close(redirs_manage->pipes[1]);
+}
+
+static int	fork_exeptions(char **command, char ***env,
+		t_redirs_manage *redirs_manage)
+{
+	if (ft_strncmp(command[0], "exit", 5) == 0 && redirs_manage->is_pipe)
+		return (redirs_manage->forced_pipe = 1, -1);
+	else if (ft_strncmp(command[0], "exit", 5) == 0)
+		return (builtin_exit(command, env));
+	if (ft_strncmp(command[0], "cd", 3) == 0 && redirs_manage->is_pipe)
+		return (redirs_manage->forced_pipe = 1, -1);
+	else if (ft_strncmp(command[0], "cd", 3) == 0)
+		return (builtin_cd(command, env));
+	return (-1);
+}
+
 int	builtin_fork(char **command, t_redirs_manage *redirs_manage, char ***env,
 		int f(char **, char ***))
 {
 	pid_t	pid;
 	int		error;
 
+	error = fork_exeptions(command, env, redirs_manage);
+	if (error != -1)
+		return (error);
 	error = 0;
 	if (create_pipe(redirs_manage, -1, redirs_manage->save_out) == -1)
 		return (1);
@@ -38,10 +63,7 @@ int	builtin_fork(char **command, t_redirs_manage *redirs_manage, char ***env,
 		if (redirs_manage->is_pipe && !redirs_manage->fds_out)
 			if (dup2(redirs_manage->pipes[1], STDOUT_FILENO) == -1)
 				ft_perror_exit("dup2 output redirection");
-		if (redirs_manage->pipes[0] != -1)
-			close(redirs_manage->pipes[0]);
-		if (redirs_manage->pipes[1] != -1)
-			close(redirs_manage->pipes[1]);
+		close_pipes(redirs_manage);
 		error = f(command, env);
 		exit(error);
 	}
