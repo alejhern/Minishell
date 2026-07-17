9d950a8d70d34702e0d9f18dbacfb96d141fd181
Alejandro Hernández Hernández <alejhern@student.42barcelona.com>
2026-07-18 01:10:34 +0200

    refactor token expansion logic for improved readability and functionality

--- parser/tokenize_expansion.c
+++ parser/tokenize_expansion.c
@@ -12,55 +12,73 @@
 
 #include "../minishell.h"
 
-static int	token_expanse_util(char p)
+static char	*token_expand_var(char *p, int *i, char **env)
 {
-	if (p != ' ' && p != '	' && p && p != '$' && p != '\n' && p != '\''
-		&& p != '\"')
-		return (1);
-	return (0);
+	char	*var;
+	char	*exp;
+	int		k;
+
+	k = 0;
+	while (ft_isalnum(p[*i + i[1] + 1 + k]) || p[*i + i[1] + 1 + k] == '_')
+		k++;
+	if (k == 0)
+	{
+		exp = ft_strdup("$");
+		i[1]++;
+	}
+	else
+	{
+		var = ft_substr(p, *i + i[1] + 1, k);
+		exp = ft_getenv(var, env);
+		free(var);
+		if (exp)
+			exp = ft_strdup(exp);
+		else
+			exp = ft_strdup("");
+		i[1] += k + 1;
+	}
+	return (exp);
 }
 
 static void	token_expanse(char *p, int *i, t_token **token, char **env)
 {
-	char	*var;
-	int		k;
 	char	*exp;
+	t_token	*next;
 
-	k = 1;
-	while (token_expanse_util(p[*i + i[1] + k]))
-		k++;
-	if (p[*i + i[1] + 1] == '$')
-		k = 2;
-	if (p[*i + i[1] + k] == '\'' || p[*i + i[1] + k] == '\"'
-		|| p[*i + i[1] + k] == '\n')
-		k--;
-	var = ft_substr(p, *i + i[1] + 1, k);
-	if (!var)
-		exit(0);
-	exp = ft_getenv(var, env);
-	free(var);
-	if (exp == 0)
-		exp = ft_strdup("");
-	else
-		exp = ft_strdup(exp);
-	if (exp == 0)
-		exit(0);
-	token_lstadd_back(token, token_lstnew(exp, 0));
-	i[1] += k;
+	if (p[*i + i[1] + 1] == '?')
+	{
+		exp = ft_itoa(g_signal);
+		if (!exp)
+			exit(0);
+		i[1] += 2;
+		next = token_lstnew(exp, 0);
+		if (!next)
+			exit(0);
+		token_lstadd_back(token, next);
+		return ;
+	}
+	exp = token_expand_var(p, i, env);
+	if (exp)
+	{
+		next = token_lstnew(exp, 0);
+		if (!next)
+			exit(0);
+		token_lstadd_back(token, next);
+	}
 }
 
 static void	token_word2(char *p, int *i, t_token **token, char **env)
 {
-	int			k;
-	t_token		*next;
+	int		k;
+	t_token	*next;
 
 	next = token_lstnew(0, 0);
 	i[1] = 0;
 	k = 0;
 	*i += (i[2] > 0);
 	token_lstadd_back(token, next);
-	while ((i[3] == 1 || ((p[*i + i[1]] != '\'' && i[2] != 2)
-				&& (p[*i + i[1]] != '\"' && i[2] != 1))) && p[*i + i[1]])
+	while ((i[3] == 1 || ((p[*i + i[1]] != '\'' || i[2] == 2) && (p[*i
+					+ i[1]] != '"' || i[2] == 1))) && p[*i + i[1]])
 	{
 		if (p[*i + i[1]] == '$' && i[2] != 1)
 		{
@@ -68,7 +86,7 @@ static void	token_word2(char *p, int *i, t_token **token, char **env)
 			token_expanse(p, i, token, env);
 			next = token_lstnew(0, 0);
 			token_lstadd_back(token, next);
-			k = i[1] + 1;
+			k = i[1];
 		}
 		else
 			i[1]++;
@@ -103,9 +121,9 @@ static char	*join_tokens(t_token *token)
 
 char	*expand(char *prompt, char **env, int doc)
 {
-	int		i[4];
-	char	*new;
-	t_token	*token;
+	int i[4];
+	char *new;
+	t_token *token;
 
 	i[0] = 0;
 	i[1] = 0;
